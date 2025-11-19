2f371816341bdf25a96b2c4ed7b65c67a7cca3f0
Dr.sheriff <kiptoobethuel0@gmail.com>
2025-11-19 15:56:48 +0300

    can't and cant

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -25,7 +25,7 @@ const Search = () => {
             const data = await fetchUserData(username);
             setUserData(data);
         } catch (err) {
-            setError("Looks like we can't find the user");
+            setError("Looks like we cant find the user");
             console.error("API Error:", err);
         } finally {
             setLoading(false);
