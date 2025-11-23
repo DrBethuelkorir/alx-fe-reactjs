190402513bbf8a5c5ba59cb9a2c58543f365d89a
Dr.sheriff <kiptoobethuel0@gmail.com>
2025-11-23 19:08:34 +0300

    corrected

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,8 +3,22 @@ import axios from 'axios'
 const SEARCH_URL = 'https://api.github.com/search/users?q='
 const USER_URL = 'https://api.github.com/users/'
 
-export const searchUsers = async (query) => {
+export const searchUsers = async (location = '', minRepos = 0) => {
     try {
+        // Build query with location and minimum repositories
+        let queryParts = [];
+        
+        if (location) {
+            queryParts.push(`location:${encodeURIComponent(location)}`);
+        }
+        
+        if (minRepos > 0) {
+            queryParts.push(`repos:>${minRepos}`);
+        }
+        
+        // If no specific filters, search for all users
+        const query = queryParts.length > 0 ? queryParts.join('+') : 'type:user';
+        
         const response = await axios.get(`${SEARCH_URL}${query}`);
         return response.data;
     } catch (error) {
